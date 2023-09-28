5ed845346d7dbd06ac8d676e53cd65e6b99870e2
RohitRole <145689440+RohitRole@users.noreply.github.com>
2023-09-28 10:23:25 +0530

    Add files via upload

---
+++ pythonstring.py
@@ -0,0 +1,45 @@
+if __name__=="__main__":
+    # # my name is jack 
+    # print('My name is "Jack"')
+    # print("My name is \"Jack\"")
+    # print('My name is \'Jack\'')
+    # print("\\n")
+
+#ex2
+    string_1="Krishna"
+    print(string_1)
+
+   # print only first 3 characters
+    print(string_1[0:3])
+    print(string_1[0:4])
+
+    print(string_1[::-1])
+
+    string_len = len(string_1)
+    print("Length pf string is %d"%string_len)
+
+#     #ex3 of string equal
+
+#     string_1 = "Jack"
+#     string_2 = "Jack"
+
+#     if string_1 == string_2:
+#         print("string are equal")
+#     else:
+#         print("string is not equal")
+
+#  #ex4 of string  not equal
+#     string_1 = "Jack"
+#     string_2 = "Mack"
+
+#     if string_1 == string_2:
+#         print("string are equal")
+#     else:
+#         print("string is not equal")
+ 
+ #ex 5
+    # string_1="Roshni\n"
+    # print(string_1 * 10)
+    # #or
+    # print("Roshni Roshni Roshni Roshni Roshni Roshni") 
+
