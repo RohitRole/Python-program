8d1235b151be2f67a2e54f367dec7f0f19d2b18f
RohitRole <145689440+RohitRole@users.noreply.github.com>
2023-11-20 17:52:24 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+
+if __name__=="__main__": 
+    list_1=[1,2,3,4,5]
+    list_2=[number + 5 for number in list_1]
+    print(list_2)
+
+    list_sq=[number ** 2 for number in list_1]
+    print(list_sq)
