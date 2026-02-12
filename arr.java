a430d2677f4752324787167cad5dfdd5cde8a1e2
RohitRole <145689440+RohitRole@users.noreply.github.com>
2026-02-12 13:41:29 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,13 @@
+public class arr
+{
+public static void main(String [] arg)
+{
+int [] marks =new int [3];
+
+marks [1]=90;
+marks [2]=85;
+System.out.println(marks[0]);
+System.out.println(marks[1]);
+System.out.println(marks[2]);
+}
+}
