72226b688188ea1b1527d5e732d60ec59e8d74c6
RohitRole <145689440+RohitRole@users.noreply.github.com>
2026-03-13 16:52:06 +0530

    Add files via upload

---
+++ arrtwo.java
@@ -0,0 +1,21 @@
+public class arrtwo
+{
+public static void main(String[] arg)
+{
+int [][] a={
+{1,2},
+{3,4}
+};
+for(int i=0;i<a.length;i++)
+{
+for(int j=0;j<a[i].length;j++)
+{
+System.out.print(a[i][j] + " ");
+}
+System.out.println();
+}
+}}
+
+// output
+// 1 2
+// 3 4
