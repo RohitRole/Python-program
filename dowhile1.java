baf223220e2e22c4699381db175ef8fb7f6cdeaf
RohitRole <145689440+RohitRole@users.noreply.github.com>
2026-04-04 11:07:03 +0530

    Add files via upload

---
+++ dowhile1.java
@@ -0,0 +1,12 @@
+public class dowhile1
+{
+public static void main(String[] arg)
+{
+int a=0;
+int[] i={1,2,2,3,4,5,6,7,4,5};
+do{
+System.out.println("do while loop values:"+ i[a]);
+a++;
+}while(a<i.length);`
+}
+}
